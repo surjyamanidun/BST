3cc19ca72b4582555eb93e7ce1d2db22db6ba13e
surjya <surjyamaniduan@gmail.com>
2021-10-12 00:08:42 +0530

    add:checking BST

--- src/main/java/binary/BSTmain.java
+++ src/main/java/binary/BSTmain.java
@@ -2,4 +2,34 @@ package binary;
 
 public class BSTmain {
 
+    public static void main(String[] args) {
+
+        BST obj = new BST();
+
+        //inserting 
+        obj.insert(56);
+        obj.insert(30);
+        obj.insert(70);
+
+        //for uc2
+        obj.insert(22);
+        obj.insert(40);
+        obj.insert(11);
+        obj.insert(16);
+        obj.insert(3);
+        obj.insert(70);
+        obj.insert(95);
+        obj.insert(60);
+        obj.insert(65);
+        obj.insert(63);
+        obj.insert(67);
+
+        
+        System.out.println("Data in the format of ascending order");
+        obj.inorder();
+
+        //uc3 searching method calling
+        obj.search(40);
+
+    }
 }
